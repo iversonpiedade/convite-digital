db66f4e5690f58fa41ea1528c272a7d08e30f676
Iverson da Piedade <105354047+iversonpiedade@users.noreply.github.com>
2022-12-03 21:45:42 +0100

    Update index.tsx with new location

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,8 +50,8 @@ const Home: NextPage = () => {
   };
 
   const location = {
-    lat: "-8.9728236",
-    long: "13.1502776",
+    lat: "-8.963322",
+    long: "13.202073",
   };
 
   const mapMapUrl = `https://www.google.com/maps/search/?api=1&query=${location.lat}%2C${location.long}`;
